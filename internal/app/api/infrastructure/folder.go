cb0de00fae9b177004baeae344575a9801bac819
atsumarukun <atys.smz@gmail.com>
2024-08-10 15:51:15 +0000

    create: FolderInfrastructureにCreate関数を作成

--- internal/app/api/infrastructure/folder.go
+++ internal/app/api/infrastructure/folder.go
@@ -1,3 +1,51 @@
 package infrastructure
 
+import (
+	"file-server/internal/app/api/domain/entity"
+	"file-server/internal/app/api/domain/repository"
+	"file-server/internal/app/api/infrastructure/model"
+
+	"gorm.io/gorm"
+)
+
 type folderInfrastructure struct{}
+
+func NewFolderInfrastructure() repository.FolderRepository {
+	return &folderInfrastructure{}
+}
+
+func (fi *folderInfrastructure) Create(db *gorm.DB, folder *entity.Folder) (*entity.Folder, error) {
+	folderModel := fi.entityToModel(folder)
+	if err := db.Create(folderModel).Error; err != nil {
+		return nil, err
+	}
+	return fi.modelToEntity(folderModel)
+}
+
+func (fi *folderInfrastructure) entityToModel(folder *entity.Folder) *model.FolderModel {
+	return &model.FolderModel{
+		ID:             folder.GetID(),
+		ParentFolderID: folder.GetParentFolderID(),
+		Name:           folder.GetName(),
+		Path:           folder.GetPath(),
+		IsHide:         folder.GetIsHide(),
+		CreatedAt:      folder.GetCreatedAt(),
+		UpdatedAt:      folder.GetUpdatedAt(),
+	}
+}
+
+func (fi *folderInfrastructure) modelToEntity(folder *model.FolderModel) (*entity.Folder, error) {
+	folderEntity := &entity.Folder{}
+	folderEntity.SetID(folder.ID)
+	folderEntity.SetParentFolderID(folder.ParentFolderID)
+	if err := folderEntity.SetName(folder.Name); err != nil {
+		return nil, err
+	}
+	if err := folderEntity.SetPath(folder.Path); err != nil {
+		return nil, err
+	}
+	folderEntity.SetIsHide(folder.IsHide)
+	folderEntity.SetCreatedAt(folder.CreatedAt)
+	folderEntity.SetUpdatedAt(folder.UpdatedAt)
+	return folderEntity, nil
+}
---
+++ internal/app/api/infrastructure/model/folder.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type FolderModel struct {
+	ID             int64
+	ParentFolderID *int64
+	Name           string
+	Path           string
+	IsHide         bool
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      gorm.DeletedAt
+}
