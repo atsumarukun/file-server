e523637094043a068adb22c216ed71c3d2b347c7
atsumarukun <atys.smz@gmail.com>
2024-08-11 16:22:38 +0000

    create: FolderBodyInfrastructureにCreate関数を作成

--- internal/app/api/infrastructure/folder_body.go
+++ internal/app/api/infrastructure/folder_body.go
@@ -1,9 +1,22 @@
 package infrastructure
 
-import "file-server/internal/app/api/domain/repository"
+import (
+	"file-server/internal/app/api/domain/entity"
+	"file-server/internal/app/api/domain/repository"
+	"file-server/internal/pkg/config"
+	"os"
+)
 
 type folderBodyInfrastructure struct{}
 
 func NewFolderBodyInfrastructure() repository.FolderBodyRepository {
 	return &folderBodyInfrastructure{}
+}
+
+func (fi *folderBodyInfrastructure) Create(folder *entity.FolderBody) error {
+	info, err := os.Lstat("./")
+	if err != nil {
+		return err
+	}
+	return os.MkdirAll(config.STORAGE_PATH+folder.GetPath(), info.Mode())
 }
