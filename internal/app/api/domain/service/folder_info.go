ee39dd5e324d2995d97a3c562fd9f7c8fd92e068
atsumarukun <atys.smz@gmail.com>
2024-08-12 09:25:35 +0000

    create: FolderInfoServiceにExists関数を作成

--- internal/app/api/domain/service/folder_info.go
+++ internal/app/api/domain/service/folder_info.go
@@ -1,10 +1,16 @@
 package service
 
 import (
+	"file-server/internal/app/api/domain/entity"
 	"file-server/internal/app/api/domain/repository"
+	"fmt"
+
+	"gorm.io/gorm"
 )
 
-type FolderInfoService interface{}
+type FolderInfoService interface {
+	Exists(*gorm.DB, *entity.FolderInfo) error
+}
 
 type folderInfoService struct {
 	folderInfoRepository repository.FolderInfoRepository
@@ -14,4 +20,14 @@ func NewFolderInfoService(folderInfoRepository repository.FolderInfoRepository) 
 	return &folderInfoService{
 		folderInfoRepository: folderInfoRepository,
 	}
+}
+
+func (fs *folderInfoService) Exists(db *gorm.DB, folder *entity.FolderInfo) error {
+	path := folder.GetPath()
+	if checkFolder, err := fs.folderInfoRepository.FindOneByPath(db, path); err != nil {
+		return err
+	} else if checkFolder != nil {
+		return fmt.Errorf("%s is already exists", path)
+	}
+	return nil
 }
