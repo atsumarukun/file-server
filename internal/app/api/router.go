66ddfbc3bef63fc93c837d90091ee9546e26ec20
atsumarukun <atys.smz@gmail.com>
2024-08-12 13:53:14 +0000

    add: route関数にフォルダ削除ルートを追加

--- internal/app/api/router.go
+++ internal/app/api/router.go
@@ -22,5 +22,6 @@ func route(r *gin.Engine, db *gorm.DB) {
 		folders.POST("/", folderHandler.Create)
 		folders.GET("/*path", folderHandler.FindOne)
 		folders.PUT("/:id", folderHandler.Update)
+		folders.DELETE("/:id", folderHandler.Remove)
 	}
 }
