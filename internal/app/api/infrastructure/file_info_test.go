ff7b692763be2ee411d2c36ab2a2cb8a74c603ee
atsumarukun <atys.smz@gmail.com>
2024-09-16 15:11:06 +0000

    create: FileInfoInfrastructureのCreates関数テストコードを作成

--- internal/app/api/infrastructure/file_info_test.go
+++ internal/app/api/infrastructure/file_info_test.go
@@ -57,3 +57,53 @@ func TestCreate(t *testing.T) {
 		t.Error("failed to insert updated_at automatically")
 	}
 }
+
+func TestCreates(t *testing.T) {
+	db, mock, err := database.Open()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	file, err := entity.NewFileInfo(1, "name", "/path/", "mime/type", false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	files := []entity.FileInfo{*file}
+
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `files` (`folder_id`,`name`,`path`,`mime_type`,`is_hide`,`created_at`,`updated_at`) VALUES (?,?,?,?,?,?,?)")).WithArgs(file.FolderID, file.Name.Value, file.Path.Value, file.MimeType.Value, file.IsHide, database.AnyTime{}, database.AnyTime{}).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	fi := NewFileInfoInfrastructure()
+
+	results, err := fi.Creates(db, files)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err.Error())
+	}
+
+	opts := []cmp.Option{
+		cmpopts.IgnoreFields(entity.FileInfo{}, "ID", "CreatedAt", "UpdatedAt"),
+	}
+
+	if diff := cmp.Diff(files, results, opts...); diff != "" {
+		t.Error(diff)
+	}
+
+	for _, result := range results {
+		if result.ID == 0 {
+			t.Error("failed to insert id automatically")
+		}
+
+		if result.CreatedAt == database.NullTime {
+			t.Error("failed to insert created_at automatically")
+		}
+
+		if result.UpdatedAt == database.NullTime {
+			t.Error("failed to insert updated_at automatically")
+		}
+	}
+}
