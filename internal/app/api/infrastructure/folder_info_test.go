430cbeeeb64329e37048707d4af0044c413c7866
atsumarukun <atys.smz@gmail.com>
2024-10-01 16:02:45 +0000

    create: FolderInfoInfrastructureのFindOneByIDWithLower関数テストコードを作成

--- internal/app/api/infrastructure/folder_info_test.go
+++ internal/app/api/infrastructure/folder_info_test.go
@@ -171,7 +171,7 @@ func TestFindOneFolderByPath(t *testing.T) {
 	}
 
 	if result == nil {
-		t.Error("failed to find the file by id")
+		t.Error("failed to find the file by path")
 	}
 }
 
@@ -197,7 +197,7 @@ func TestFindOneFolderByPathWithChildren(t *testing.T) {
 	}
 
 	if result == nil {
-		t.Error("failed to find the file by id")
+		t.Error("failed to find the file by path with children")
 	}
 }
 
@@ -223,6 +223,35 @@ func TestFindOneFolderByPathAndIsHideWithChildren(t *testing.T) {
 	}
 
 	if result == nil {
-		t.Error("failed to find the file by id")
+		t.Error("failed to find the file by path and is_hide with children")
+	}
+}
+
+func TestFindOneFolderByIDWithLower(t *testing.T) {
+	db, mock, err := database.Open()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `folders` WHERE id = ? ORDER BY `folders`.`id` LIMIT ?")).WithArgs(1, 1).WillReturnRows(sqlmock.NewRows([]string{"id", "parent_folder_id", "name", "path", "is_hide", "created_at", "updated_at"}).AddRow(1, 1, "name", "/path/", false, time.Now(), time.Now()))
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `files` WHERE `files`.`folder_id` = ?")).WithArgs(1).WillReturnRows(sqlmock.NewRows([]string{"id", "folder_id", "name", "path", "mime_type", "is_hide", "created_at", "updated_at"}).AddRow(1, 1, "name", "/path/", "mime/type", false, time.Now(), time.Now()))
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `folders` WHERE `folders`.`parent_folder_id` = ?")).WithArgs(1).WillReturnRows(sqlmock.NewRows([]string{"id", "parent_folder_id", "name", "path", "is_hide", "created_at", "updated_at"}).AddRow(1, 1, "name", "/path/", false, time.Now(), time.Now()))
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `folders` WHERE id = ? ORDER BY `folders`.`id` LIMIT ?")).WithArgs(1, 1).WillReturnRows(sqlmock.NewRows([]string{"id", "parent_folder_id", "name", "path", "is_hide", "created_at", "updated_at"}).AddRow(1, 1, "name", "/path/", false, time.Now(), time.Now()))
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `files` WHERE `files`.`folder_id` = ?")).WithArgs(1).WillReturnRows(sqlmock.NewRows(nil))
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `folders` WHERE `folders`.`parent_folder_id` = ?")).WithArgs(1).WillReturnRows(sqlmock.NewRows(nil))
+
+	fi := NewFolderInfoInfrastructure()
+
+	result, err := fi.FindOneByIDWithLower(db, 1)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err.Error())
+	}
+
+	if result == nil {
+		t.Error("failed to find the file by id with lower")
 	}
 }
