8ae7b89362ff6b4d35e8dbd859ce69faf7c0372e
atsumarukun <atys.smz@gmail.com>
2024-10-02 14:21:51 +0000

    create: AuthUsecaseのSignin関数テストコードを作成

---
+++ internal/app/api/usecase/auth_test.go
@@ -0,0 +1,40 @@
+package usecase
+
+import (
+	"file-server/internal/app/api/domain/entity"
+	"file-server/test/database"
+	mock_repository "file-server/test/mock/repository"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestSignin(t *testing.T) {
+	db, _, err := database.Open()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	credential := entity.NewCredential(string(hash))
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repo := mock_repository.NewMockCredentialRepository(ctrl)
+	repo.EXPECT().FindOne(db).Return(credential, err)
+
+	au := NewAuthUsecase(db, repo)
+	result, err := au.Signin("password")
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if result == nil {
+		t.Error("failed to signin")
+	}
+}
