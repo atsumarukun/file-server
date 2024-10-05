9c9c151b7fb5f1b1129662b8c2e5952c836e85cd
atsumarukun <atys.smz@gmail.com>
2024-10-05 10:42:55 +0000

    create: FolderUsecaseのUpdate関数テストコードを作成

--- internal/app/api/usecase/folder_test.go
+++ internal/app/api/usecase/folder_test.go
@@ -47,3 +47,44 @@ func TestCreateFolder(t *testing.T) {
 		t.Error("failed to create folder")
 	}
 }
+
+func TestUpdateFolder(t *testing.T) {
+	db, mock, err := database.Open()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	mock.ExpectBegin()
+	mock.ExpectCommit()
+
+	folderInfo, err := entity.NewFolderInfo(nil, "name", "/path/name/", false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	folderInfo.ID = 2
+	var parentFolderID uint64 = 1
+	folderInfo.ParentFolderID = &parentFolderID
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	folderInfoRepository := mock_repository.NewMockFolderInfoRepository(ctrl)
+	folderInfoRepository.EXPECT().FindOneByIDAndIsHideWithLower(gomock.Any(), gomock.Any(), gomock.Any()).Return(folderInfo, nil)
+	folderInfoRepository.EXPECT().Update(gomock.Any(), gomock.Any()).Return(folderInfo, nil)
+
+	folderBodyRepository := mock_repository.NewMockFolderBodyRepository(ctrl)
+	folderBodyRepository.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
+
+	folderInfoService := mock_service.NewMockFolderInfoService(ctrl)
+	folderInfoService.EXPECT().IsExists(gomock.Any(), gomock.Any()).Return(false, nil)
+
+	fu := NewFolderUsecase(db, folderInfoRepository, folderBodyRepository, folderInfoService)
+
+	result, err := fu.Update(folderInfo.ID, "update", false, false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if result == nil {
+		t.Error("failed to update folder")
+	}
+}
