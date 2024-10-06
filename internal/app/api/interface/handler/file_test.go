514fc650d89833b4030f3dc50c419294ec7d3d86
atsumarukun <atys.smz@gmail.com>
2024-10-06 08:45:29 +0000

    create: FileHandlerのRemove関数テストコードを作成

--- internal/app/api/interface/handler/file_test.go
+++ internal/app/api/interface/handler/file_test.go
@@ -94,7 +94,7 @@ func TestUpdateFile(t *testing.T) {
 		t.Error(err.Error())
 	}
 
-	req, err := http.NewRequest("POST", "/files/1", bytes.NewBuffer(body))
+	req, err := http.NewRequest("PUT", "/files/1", bytes.NewBuffer(body))
 	if err != nil {
 		t.Error(err.Error())
 	}
@@ -124,6 +124,34 @@ func TestUpdateFile(t *testing.T) {
 	fh := NewFileHandler(fu)
 
 	fh.Update(ctx)
+
+	if w.Code != http.StatusOK {
+		t.Error(w.Body.String())
+	}
+}
+
+func TestRemoveFile(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	req, err := http.NewRequest("DELETE", "/files/1", nil)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+	ctx.Request = req
+	ctx.Params = append(ctx.Params, gin.Param{Key: "id", Value: strconv.Itoa(1)})
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	fu := mock_usecase.NewMockFileUsecase(ctrl)
+	fu.EXPECT().Remove(gomock.Any(), gomock.Any()).Return(nil)
+
+	fh := NewFileHandler(fu)
+
+	fh.Remove(ctx)
 
 	if w.Code != http.StatusOK {
 		t.Error(w.Body.String())
